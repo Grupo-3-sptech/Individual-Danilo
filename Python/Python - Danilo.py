e9dc23cf1a0e993dfd92fbb88af1d609cada1cd0
Danis442 <danilo.faustino@sptech.school>
2023-11-24 15:47:06 -0300

    python indiv

---
+++ Python/Python - Danilo.py
@@ -0,0 +1,169 @@
+
+from mysql.connector import connect
+import psutil
+import platform
+import time
+import mysql.connector
+from datetime import datetime
+import ping3
+import json
+import requests
+
+#alerta = {"text": "alerta"}
+
+webhook = "https://hooks.slack.com/services/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"
+#requests.post(webhook, data=json.dumps(alerta))
+
+
+idRobo = 1
+
+#descomente abaixo quando for ora criar esse arquivo peo kotlin
+# idRobo = #${roboId}
+
+
+
+def mysql_connection(host, user, passwd, database=None):
+    connection = connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        database=database
+    )
+    return connection
+
+def bytes_para_gb(bytes_value):
+    return bytes_value / (1024 ** 3)
+
+def milissegundos_para_segundos(ms_value):
+    return ms_value / 1000
+
+connection = mysql_connection('localhost', 'medconnect', 'medconnect123', 'medconnect')
+
+#Disco
+
+meu_so = platform.system()
+if(meu_so == "Linux"):
+    nome_disco = '/'
+    disco = psutil.disk_usage(nome_disco)
+elif(meu_so == "Windows"):
+    nome_disco = 'C:\\'
+disco = psutil.disk_usage(nome_disco)
+discoPorcentagem = disco.percent
+
+ins = [discoPorcentagem]
+componentes = [10,11,12,13,14]
+
+horarioAtual = datetime.now()
+horarioFormatado = horarioAtual.strftime('%Y-%m-%d %H:%M:%S')
+
+cursor = connection.cursor()
+for i in range(len(ins)):
+        
+    dado = ins[i]
+        
+    componente = componentes[i]
+    
+    query = "INSERT INTO Registros (dado, fkRoboRegistro, fkComponente, HorarioDado) VALUES (%s, %s, %s, %s)"
+    
+    cursor.execute(query, (dado, idRobo, componente, horarioFormatado))
+
+
+
+print("\nDisco porcentagem:", discoPorcentagem)
+
+
+while True:
+
+    #CPU
+    cpuPorcentagem = psutil.cpu_percent(None)
+    
+    if(cpuPorcentagem > 60 and cpuPorcentagem > 70):
+        alerta = {"text": f"alerta na cpu da maquina: {idRobo} está em estado de alerta"}
+        requests.post(webhook, data=json.dumps(alerta))
+    if(cpuPorcentagem > 70 and cpuPorcentagem > 80):
+        alerta = {"text": f"alerta na cpu da maquina: {idRobo} está em estado critico"}
+        requests.post(webhook, data=json.dumps(alerta))
+    if(cpuPorcentagem > 80):
+        alerta = {"text": f"alerta na cpu da maquina: {idRobo} está em estado de urgencia"}
+        requests.post(webhook, data=json.dumps(alerta))
+        
+
+
+
+    
+    #Memoria
+    memoriaPorcentagem = psutil.virtual_memory()[2]
+   
+    if(memoriaPorcentagem > 60 and memoriaPorcentagem > 70):
+        alerta = {"text": f"⚠️  Alerta na ram da maquina: {idRobo} está em estado de alerta"}
+        requests.post(webhook, data=json.dumps(alerta))
+    if(memoriaPorcentagem > 70 and memoriaPorcentagem > 80):
+        alerta = {"text": f"⚠️  Alerta na ram da maquina: {idRobo} está em estado critico"}
+        requests.post(webhook, data=json.dumps(alerta))  
+    if(memoriaPorcentagem > 80):
+        alerta = {"text": f" ⚠️  Alerta na ram da maquina: {idRobo} está em estado de urgencia"}
+        requests.post(webhook, data=json.dumps(alerta))
+   
+    #Rede
+        
+    destino = "google.com"  
+    latencia = ping3.ping(destino) * 1000
+    if(latencia > 40 and latencia > 60):
+        alerta = {"text": f"⚠️Alerta no ping da maquina: {idRobo} está em estado de alerta"}
+        requests.post(webhook, data=json.dumps(alerta))
+    if(latencia > 60 and latencia > 80):
+        alerta = {"text": f"⚠️Alerta no ping da maquina: {idRobo} está em estado critico"}
+        requests.post(webhook, data=json.dumps(alerta))
+    if(latencia > 80):
+        alerta = {"text": f"⚠️Alerta no ping da maquina: {idRobo} está em estado de urgencia"}
+        requests.post(webhook, data=json.dumps(alerta))
+    
+    if latencia is not None:
+        print(f"Latência para {destino}: {latencia:.2f} ms")
+    else:
+        print(f"Não foi possível alcançar {destino}")  
+
+    
+    if network_active:
+
+        print ("A rede está ativa.")
+        statusRede= 1
+    else:
+
+        print ("A rede não está ativa.")
+
+    #Outros
+    boot_time = datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    print("A maquina está ligada desde: ",boot_time)
+
+    horarioAtual = datetime.now()
+    horarioFormatado = horarioAtual.strftime('%Y-%m-%d %H:%M:%S')
+    
+    ins = [cpuPorcentagem, memoriaPorcentagem, latencia]
+    componentes = [1,2,3,4,5,6,7,8,9,15,16,17,18]
+    
+    cursor = connection.cursor()
+    
+    for i in range(len(ins)):
+        dado = ins[i]
+        componente = componentes[i]
+
+        query = "INSERT INTO Registros (dado, fkRoboRegistro, fkComponente, HorarioDado) VALUES (%s, %s, %s, %s)"
+
+        cursor.execute(query, (dado, idRobo, componente, horarioFormatado))
+        connection.commit()
+
+       
+    print("\nINFORMAÇÕES SOBRE PROCESSAMENTO: ")
+    print('\nPorcentagem utilizada da CPU: ',cpuPorcentagem,
+          '\nPorcentagem utilizada de memoria: ', memoriaPorcentagem)
+   
+    
+       
+
+
+    time.sleep(5)
+
+cursor.close()
+connection.close()
+    
